c40e2799852abfb4c2d0e6252e8b9236063e6cc6
Luizgustavo358 <luiz.gustavo358@gmail.com>
2021-12-24 08:54:16 -0300

    detalhes do produto

--- vendor/hcodebr/php-classes/src/Model/Product.php
+++ vendor/hcodebr/php-classes/src/Model/Product.php
@@ -110,6 +110,30 @@ class Product extends Model {
 
         $this->checkPhoto();
     }
+
+    public function getFromURL($desurl) {
+        $sql = new Sql();
+
+        $rows = $sql->select("SELECT * FROM TB_PRODUCTS WHERE DESURL = :desurl LIMIT 1", [
+            ':desurl' => $desurl
+        ]);
+
+        $this->setData($rows[0]);
+    }
+
+    public function getCategories() {
+        $sql = new Sql();
+
+        return $sql->select("
+            SELECT *
+                FROM tb_categories a 
+                    INNER JOIN tb_productscategories b
+                        ON a.idcategory = b.idcategory
+                WHERE b.idproduct = :idproduct
+        ", [
+            ":idproduct" => $this->getidproduct()
+        ]);
+    }
 }
 
 ?>
