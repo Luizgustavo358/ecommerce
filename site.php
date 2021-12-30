09b753bfe174d46c36d868374b2c549f9d94cd0a
Luizgustavo358 <luiz.gustavo358@gmail.com>
2021-12-30 14:03:14 -0300

    cart

--- site.php
+++ site.php
@@ -3,6 +3,7 @@
 use \Hcode\Page;
 use \Hcode\Model\Product;
 use \Hcode\Model\Category;
+use \Hcode\Model\Cart;
 
 // index
 $app->get('/', function() {
@@ -51,6 +52,13 @@ $app->get("/products/:desurl", function($desurl) {
         'product' => $product->getValues(),
         'categories' => $product->getCategories()
     ]);
+});
+
+$app->get("/cart", function() {
+    $cart = Cart::getFromSession();
+
+    $page = new Page();
+    $page->setTpl("cart");
 });
 
 ?>
