cf7cbb74e4367eaf44d80a0a080b085daafdbed5
Luizgustavo358 <luiz.gustavo358@gmail.com>
2022-02-04 17:29:47 -0300

    minha conta

--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -43,7 +43,11 @@ class User extends Model {
     public static function login($login, $password) {
         $sql = new Sql();
 
-        $results = $sql->select("SELECT * FROM tb_users WHERE deslogin = :LOGIN", array(
+        $results = $sql->select("SELECT * 
+                                    FROM tb_users a 
+                                    INNER JOIN tb_persons b 
+                                        ON a.idperson = b.idperson 
+                                    WHERE a.deslogin = :LOGIN", array(
             ":LOGIN" => $login
         ));
 
