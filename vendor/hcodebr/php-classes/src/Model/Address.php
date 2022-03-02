0b093aefdd650d3436cdabefabcb17f7316f447a
Luizgustavo358 <luiz.gustavo358@gmail.com>
2022-03-02 13:25:02 -0300

    ajuste endereço

--- vendor/hcodebr/php-classes/src/Model/Address.php
+++ vendor/hcodebr/php-classes/src/Model/Address.php
@@ -40,15 +40,16 @@ class Address extends Model {
 
     public function save() {
         $sql = new Sql();
-        $results = $sql->select("CALL sp_addresses_save(:idaddress, :idperson, :desaddress, :descomplement, 
+        $results = $sql->select("CALL sp_addresses_save(:idaddress, :idperson, :desaddress, :desnumber, :descomplement, 
                                                         :descity, :desstate, :descountry, :deszipcode, :desdistrict)", [
             ':idaddress'     => $this->getidaddress(),
             ':idperson'      => $this->getidperson(),
-            ':desaddress'    => $this->getdesaddress(),
-            ':descomplement' => $this->getdescomplement(),
-            ':descity'       => $this->getdescity(),
-            ':desstate'      => $this->getdesstate(),
-            ':descountry'    => $this->getdescountry(),
+            ':desaddress'    => utf8_decode($this->getdesaddress()),
+            ':desnumber'     => $this->getdesnumber(),
+            ':descomplement' => utf8_decode($this->getdescomplement()),
+            ':descity'       => utf8_decode($this->getdescity()),
+            ':desstate'      => utf8_decode($this->getdesstate()),
+            ':descountry'    => utf8_decode($this->getdescountry()),
             ':deszipcode'    => $this->getdeszipcode(),
             ':desdistrict'   => $this->getdesdistrict()
         ]);
